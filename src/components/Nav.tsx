2f82d0b676198eb7493d25804fde3ca8726ab045
vluu11 <luuvuong11@gmail.com>
2024-11-17 20:35:11 -0800

    added nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,29 @@
+import { NavLink } from 'react-router-dom';
+
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/SavedCandidates"
+            className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}
+          >
+            Potential Candidates
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
