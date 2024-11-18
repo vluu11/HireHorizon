a40a2090b41c35fa4da63a1ad741b38578403509
vluu11 <luuvuong11@gmail.com>
2024-11-17 20:53:26 -0800

    interface added

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+export interface Candidate {
+    login: string;
+    name: string | null;
+    location: string | null;
+    avatar_url: string;
+    email: string | null;
+    html_url: string;
+    company: string | null;
+    bio: string;
+  }
+  
